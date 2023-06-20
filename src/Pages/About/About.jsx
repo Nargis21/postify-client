99d4bff42ccfc89c13085d9abe38e6af7f15348c
nargis21 <nargisakther1933@gmail.com>
2023-06-20 14:15:14 +0600

    content updated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1 className="text-red-600">About Page</h1>
+    <div className="mt-20">
+      <div className="card card-side bg-base-100 shadow-xl w-[50%] mx-auto">
+        <div className="card-body">
+          <p>Name: Nargis Akther</p>
+          <p>Email: nargisakther1933@gmail.com</p>
+          <p>Institute: Chittagong Mohila Polytechnic Institute</p>
+          <p>Address: North Patenga, Chattogram</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
@@ -9,6 +9,7 @@ import Post from "../Shared/Post";
 import Loading from "../Shared/Loading";
 
 const Home = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
@@ -84,67 +85,85 @@ const Home = () => {
   }
 
   return (
-    <div className="">
-      <div className="flex flex-col justify-center items-center text-center backCol">
-        <h1 className="text-2xl meriFont pb-6 text-center ">
-          Profile Information
-        </h1>
-        <div className="pb-10 lg:w-6/12 md:w-8/12 w-full">
-          <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-control w-full max-w-x">
-              <label className="label">
-                <span className="label-text text-gray-600 font-semibold">
-                  Text
-                </span>
-              </label>
-              <textarea
-                rows="10"
-                cols="10"
-                className="textarea textarea-warning w-full max-w-x mb-4"
-                placeholder="Put your text here"
-                type="text"
-                {...register("text", { required: true })}
-              ></textarea>
-              <label className="label">
-                {errors.text && (
-                  <p className="text-red-500 text-sm">Please enter some text</p>
-                )}
-              </label>
-            </div>
-            <div className="form-control w-full max-w-x">
-              <label className="label">
-                <span className="label-text text-gray-600 font-semibold">
-                  Photo
-                </span>
-              </label>
+    <div className="w-full px-8">
+      <h1 className="text-3xl font-bold text-secondary my-8 text-center">
+        Express Your Thoughts
+      </h1>
+      <div className="flex w-full items-start my-12">
+        <div className="w-[70%] flex flex-col justify-center items-center text-center backCol">
+          <div className="pb-6 lg:w-8/12 md:w-6/12 w-full">
+            <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-control w-full max-w-x">
+                <input
+                  type="file"
+                  placeholder="Image"
+                  className="input input-bordered input-warning w-full max-w-x "
+                  {...register("image", { required: true })}
+                  onChange={(e) => {
+                    setSelectedImage(URL.createObjectURL(e.target.files[0]));
+                  }}
+                />
+                <label className="label">
+                  {errors.image && (
+                    <p className="text-red-500 text-sm">
+                      Please select an image
+                    </p>
+                  )}
+                </label>
+              </div>
+              <div className="form-control w-full max-w-x">
+                <textarea
+                  rows="10"
+                  cols="10"
+                  className="textarea textarea-warning w-full max-w-x"
+                  placeholder="Add your text here"
+                  type="text"
+                  {...register("text", { required: true })}
+                ></textarea>
+                <label className="label">
+                  {errors.text && (
+                    <p className="text-red-500 text-sm">
+                      Please enter some text
+                    </p>
+                  )}
+                </label>
+              </div>
               <input
-                type="file"
-                placeholder="Image"
-                className="input input-bordered input-warning w-full max-w-x mb-4"
-                {...register("image", { required: true })}
+                className="bg-primary p-2 rounded-lg text-white font-semibold w-full  cursor-pointer"
+                name="submit"
+                type="submit"
+                value="Upload Post"
               />
-              <label className="label">
-                {errors.image && (
-                  <p className="text-red-500 text-sm">Please select an image</p>
-                )}
-              </label>
+            </form>
+          </div>
+        </div>
+        <div className="w-[30%]">
+          <p className="text-2xl font-bold text-black mb-4">Image Preview:</p>
+          {!selectedImage && (
+            <div className="avatar">
+              <div className="w-80 rounded">
+                <img
+                  src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
+                  alt="Selected"
+                  className="max-w-xs"
+                />
+              </div>
             </div>
-            {/* <input type="hidden" {...register("reaction")} value={0} />
-            <input
-              type="hidden"
-              {...register("comment")}
-              value={JSON.stringify([])}
-            /> */}
-            <input
-              className="bg-red-900 p-2 rounded-lg text-white font-semibold w-full max-w-sm cursor-pointer"
-              name="submit"
-              type="submit"
-              value="Upload Post"
-            />
-          </form>
+          )}
+          {selectedImage && (
+            <div className="avatar">
+              <div className="w-80 rounded">
+                <img src={selectedImage} alt="Selected" className="max-w-xs" />
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-8">
+
+      <p className="text-3xl font-bold text-black mt-20 mb-10 text-center">
+        Popular Trends
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  mb-20">
         {posts.map((post) => (
           <Post key={post._id} post={post} refetch={refetch}></Post>
         ))}
